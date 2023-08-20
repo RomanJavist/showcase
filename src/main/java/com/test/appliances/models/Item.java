a120ce633fc1e0f2634e1cd42baa62aec8c3b0bb
Roman Javist <roman.javist@gmail.com>
2023-08-20 12:30:20 +0700

    Annotations

--- src/main/java/com/test/appliances/models/Item.java
+++ src/main/java/com/test/appliances/models/Item.java
@@ -1,14 +1,37 @@
 package com.test.appliances.models;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Item")
 public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int showcase_id;
+    @ManyToOne
+    @JoinColumn(name = "showcase_id", referencedColumnName = "id")
+    private Showcase sc_items;
+    @Column(name = "position")
     private int position;
+    @Column(name = "name")
+    @NotEmpty(message = "Cannot be empty")
     private String name;
+    @Column(name = "type")
     private ItemType type;
+    @Column(name = "price")
+    @NotEmpty(message = "Cannot be empty")
     private double price;
+    @Column(name = "date_create")
     private Date date_create;
+    @Column(name = "date_edit")
     private Date date_edit;
 }
--- src/main/java/com/test/appliances/models/Showcase.java
+++ src/main/java/com/test/appliances/models/Showcase.java
@@ -1,14 +1,57 @@
 package com.test.appliances.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 import java.util.Date;
-
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Showcase")
 public class Showcase {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToMany(mappedBy = "sc_items")
+    private List<Item> items;
+    @Column(name = "name")
+    @NotEmpty(message = "Cannot be empty")
     private String name;
-
+    @Column(name = "address")
+    @NotEmpty(message = "Cannot be empty")
     private String address;
-
+    @Column(name = "type")
     private ShowcaseType type;
+    @Column(name = "date_create")
     private Date date_create;
+    @Column(name = "date_edit")
     private Date date_edit;
+
+    private LocalDateTime dateTimeOfCreated;
+    private void init(){
+        dateTimeOfCreated = LocalDateTime.now();
+    }
+
+    private Date lastUpdateAt;
+
+    @PrePersist
+    public void recordUpdateTime(){
+        lastUpdateAt = new Date();
+    }
+
+    public LocalDateTime getDateTimeOfCreated() {
+        return dateTimeOfCreated;
+    }
+
+    public void setDateTimeOfCreated(LocalDateTime dateTimeOfCreated) {
+        this.dateTimeOfCreated = dateTimeOfCreated;
+    }
 }
